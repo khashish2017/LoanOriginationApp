2f7bdd480eadc796014221ee1f584d816c3a180c
khandelwala <ashishk.khandelwal1@gmail.com>
2017-02-26 13:02:54 -0600

    first commit(Basic Structure)

---
+++ ProjectLOS/app/_root/route.js
@@ -0,0 +1,27 @@
+﻿(function() {
+    'use strict';
+
+    angular
+		.module('los')
+		.config(stateConfig);
+    function stateConfig($stateProvider, $urlRouterProvider, $locationProvider) {
+        $locationProvider.hashPrefix('');
+        $urlRouterProvider.otherwise("/main");
+        $stateProvider
+    .state('main', {
+        url: '',
+        views: {
+            'Header@': {
+                templateUrl: 'app/_layout/header.html'
+            },
+            'Content@': {
+            templateUrl: 'app/_layout/content.html'
+            },
+            'Footer@': {
+            templateUrl: 'app/_layout/footer.html'
+        }
+        }
+    })
+    }
+
+})();
