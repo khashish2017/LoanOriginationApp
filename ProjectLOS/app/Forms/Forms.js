89530b0e7dcb9025c384f40618266e5a8c420f55
khandelwala <ashishk.khandelwal1@gmail.com>
2017-02-26 23:07:03 -0600

    Updated CSS

--- ProjectLOS/app/Forms/Forms.js
+++ ProjectLOS/app/Forms/Forms.js
@@ -4,8 +4,9 @@
     angular.module('los')
         .directive('contactUsCtrl', abc)
  
-    function abc() {        function ContactCtrl($scope) {            var vm = $scope;            vm.ContactReason = "Contact Reason new";            vm.fnsubmit = function ()
+    function abc() {        function ContactCtrl($scope) {            var vm = $scope;            vm.fm = [];            vm.ContactReason = "Contact Reason new";            vm.fnsubmit = function ()
             {
+                vm.submitted = true;
                 debugger;
                 alert('Submit alert');            }                   }        return {            restrict: 'A',            template: '<div ng-transclude></div>',            transclude: true,            controller: ContactCtrl,            bindToController: true        };    }
 
