69c7d560b20675191ec194480d64ade4165e1973
sunilyadav2000 <sunilyadav2000@rediffmail.com>
2017-03-01 19:33:07 -0600

    changes

--- ProjectLOS/app/Forms/Forms.js
+++ ProjectLOS/app/Forms/Forms.js
@@ -4,11 +4,31 @@
     angular.module('los')
         .directive('contactUsCtrl', abc)
  
-    function abc() {        function ContactCtrl($scope) {            var vm = $scope;            vm.fm = [];            vm.ContactReason = "Contact Reason new";            vm.fnsubmit = function ()
+    function abc() {
+        function ContactCtrl($scope) {
+            var vm = $scope;
+            vm.fm = [];
+            vm.QueryType = ["Car Loan Inquiry", "Home Loan Inquiry","Personal Loan Inquiry", "Others"]
+            //debugger;
+            //vm.FullName = "Contact Reason new";
+            vm.fnsubmit = function ()
             {
                 vm.submitted = true;
                 debugger;
-                alert('Submit alert');            }                   }        return {            restrict: 'A',            template: '<div ng-transclude></div>',            transclude: true,            controller: ContactCtrl,            bindToController: true        };    }
+                alert('Submit alert');
+                $http.post(vm);
+            }
+           
+
+        }
+        return {
+            restrict: 'A',
+            template: '<div ng-transclude></div>',
+            transclude: true,
+            controller: ContactCtrl,
+            bindToController: true
+        };
+    }
 
     
 })();
---
+++ WebAPI/Model/ContactUs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model
+{
+  
+public class ContactUs
+{
+   public int Id { get; set; }
+   public string Title { get; set; }
+   public string Author { get; set; }
+   public DateTime PublishedDate { get; set; }
+}
+
+ 
+}
